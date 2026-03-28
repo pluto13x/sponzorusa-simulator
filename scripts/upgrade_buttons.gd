6472ba5beca7b25101c1aebebbba2e9d3685b9a9
pluto13x <saramilosavljevic6@gmail.com>
2026-03-28 18:16:40 +0100

    buttons change tiers

--- scripts/upgrade_buttons.gd
+++ scripts/upgrade_buttons.gd
@@ -24,7 +24,7 @@ func _ready() -> void:
 	lip_filler.change_data(0, 0, "#LipFiller", "Get big, luscious lips 💋", "-$500", lip_img,  0, 500)
 	boob_job.change_data(1, 0, "#BoobJob", "Upgrade your look... twice 👀", "-$1000", boob_img, 0, 1000)
 	hair.change_data(2, 0, "#SilkyHair", "Get rid of the frizz 👱‍♀️", "-$100", hair_img, 0, 100)
-	makeup.change_data(3, 0, "#MakeUp", "Buy some makeup and DIY 💄", "-$50", makeup_img, 0, 50)
+	makeup.change_data(3, 0, "#LashExtensions", "Get voluminous lashes 👁️", "-$50", makeup_img, 0, 50)
 	
 	lip_filler.pressed_upgrade.connect(_on_upgrade_pressed)
 	boob_job.pressed_upgrade.connect(_on_upgrade_pressed)
@@ -35,10 +35,24 @@ func _on_upgrade_pressed(idx, idy) -> void:
 	to_sponzorusa.emit(idx, idy)
 	match idx:
 		0: #lips
-			print("Lip filler clicked")
+			match idy:
+				0:
+					lip_filler.change_data(0, 1, "#LipFiller", "Make em kissable 😘", "-$500", lip_img,  0, 500)
+				1:
+					lip_filler.change_data(0, 2, "#LipFiller", "Big, JUICY lips 💋", "-$500", lip_img,  0, 500)
 		1: #boobs
-			print("Boob job clicked")
+			match idy:
+				0:
+					boob_job.change_data(1, 1, "#BoobJob", "Get them melons 🍈", "-$1000", boob_img, 0, 1000)
+				1:
+					boob_job.change_data(1, 2, "#BoobJob", "Give em gravity 🌍", "-$1000", boob_img, 0, 1000)
 		2: #hair
-			print("Hair clicked")
+			match idy:
+				0:
+					hair.change_data(2, 1, "#BlondeHair", "Just like Rapunzel 👱‍♀️", "-$100", hair_img, 0, 100)
 		3: #makeup
-			print("Makeup clicked")
+			match idy:
+				0:
+					makeup.change_data(3, 1, "#JimmyMUA", "See a famous MUA", "-$500", makeup_img, 0, 500)
+				1: 
+					makeup.change_data(3, 2, "#TattooMakeup", "Be beautiful, always", "-$50", makeup_img, 0, 50)
